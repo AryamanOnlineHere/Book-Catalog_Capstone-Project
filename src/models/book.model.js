6ac7765a82b3f702066b3aaffdeca5d5ab75542b
AryamanOnlineHere <Aryaman.Mahato@cognizant.com>
2025-09-25 23:06:22 +0530

    added express validator

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -1,10 +1,19 @@
-const mongoose =require("mongoose");
-const Book =mongoose.model( "Book",new mongoose.Schema({
-        title:{type : String, required:true,unique:true},
-        author:String,
-        genre:String,
-        year:Number,
-        description:String
-    })
-)
-module.exports=Book;
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true,trim:true },
+  author: String,
+  genre: String,
+  year: Number,
+  description: String
+});
+bookSchema.pre("save", function (next) {
+  if (this.title) {
+    this.title = this.title.toLowerCase();
+  }
+  next();
+});
+bookSchema.index({ title: 1 }, { unique: true, collation: { locale: 'en', strength: 2 } });
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,6 +1,49 @@
 const controller= require("../controllers/book.controller");
+const {body,validationResult}= require('express-validator');
 const path="/api/books";
+const validate=validations=>{
+    return async(request,response,next)=>{
+        for(const validation of validations){
+            const result =await validation.run(request);
+            if(!result.isEmpty()){
+                return response.status(400).json({errors: result.array()});
+            }
+        }
+        next();
+    };
+};
+
 module.exports=function(app){
-    app.post(path,controller.add);
+    app.post(path,validate([body('title').notEmpty().withMessage('Title is required').isLength({max:10}).withMessage('Title connot exceed 10 charecter')]),controller.add);
     app.get(path,controller.getall);
 }
+
+// const controller = require("../controllers/book.controller");
+// const { body, validationResult } = require('express-validator');
+// const path = "/api/books";
+
+// const validate = (validations) => {
+//     return async (request, response, next) => {
+//         for (const validation of validations) {
+//             const result = await validation.run(request);
+//             if (!result.isEmpty()) {
+//                 return response.status(400).json({ errors: result.array() });
+//             }
+//         }
+//         next();
+//     };
+// };
+
+// module.exports = function(app) {
+//     app.post(
+//         path,
+//         validate([
+//             body('title')
+//                 .notEmpty().withMessage('Title is required')
+//                 .isLength({ max: 10 }).withMessage('Title cannot exceed 10 characters')
+//         ]),
+//         controller.add
+//     );
+
+//     app.get(path, controller.getall);
+// };
