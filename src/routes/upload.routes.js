5c7e2b3fbc17596b37ef4df1bc93d0dc9bbaa2af
AryamanOnlineHere <Aryaman.Mahato@cognizant.com>
2025-10-10 09:37:34 +0530

    added role based auth

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/upload.controller");
 const upload=require("../middlewares/uploadFile");
-const { verifyToken, allowAuthorOrAdmin } = require("../middlewares/authJwt");
+const { verifyToken,isAuthorOrAdmin } = require("../middlewares/authJwt");
 
 module.exports = (app) => {
-  router.post("/books/:bookId/image", verifyToken,allowAuthorOrAdmin,upload,controller.uploadBookImage);
+  router.post("/books/:bookId/image", verifyToken,isAuthorOrAdmin,upload,controller.uploadBookImage);
   router.get("/books/:bookId/image/:name", controller.downloadImage);
-  router.delete("/books/:bookId/image/:name",controller.removeImage);
-  router.get("/books/image",controller.getListFiles);
+  router.delete("/books/:bookId/image/:name", verifyToken,isAuthorOrAdmin,controller.removeImage);
+  router.get("/books/image", verifyToken,isAuthorOrAdmin,controller.getListFiles);
   app.use(router); 
 };
