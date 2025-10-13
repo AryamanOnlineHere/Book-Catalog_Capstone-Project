e881fb0f7a5ff520309d19080216bfd73026e7bd
AryamanOnlineHere <Aryaman.Mahato@cognizant.com>
2025-10-13 21:48:03 +0530

    updated getallImage

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -8,6 +8,6 @@ module.exports = (app) => {
   router.post("/books/:bookId/image", verifyToken,isAuthorOrAdmin,upload,controller.uploadBookImage);
   router.get("/books/:bookId/image/:name", controller.downloadImage);
   router.delete("/books/:bookId/image/:name", verifyToken,isAuthorOrAdmin,controller.removeImage);
-  router.get("/books/image", verifyToken,isAuthorOrAdmin,controller.getListFiles);
+  router.get("/books/image", verifyToken,controller.getListFiles);
   app.use(router); 
 };
